85024617ac5e0bda10859eab4a5770dfdbbcd20d
kubrickcode <kubrickcode@gmail.com>
2025-08-19 14:09:37 +0000

    Remove unnecessary indents

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -12,11 +12,9 @@ export const activate = (context: vscode.ExtensionContext) => {
       }
 
       const position = editor.selection.active;
-      const currentLine = editor.document.lineAt(position.line);
-      const indentation = getIndentation(currentLine.text);
 
       const tlogSnippet = new vscode.SnippetString(
-        `${indentation}console.log('[TLOG] \${1:message}');\${0}`
+        `console.log('[TLOG] \${1:message}');\${0}`
       );
 
       editor.insertSnippet(tlogSnippet, position);
@@ -24,11 +22,6 @@ export const activate = (context: vscode.ExtensionContext) => {
   );
 
   context.subscriptions.push(insertTlogCommand);
-};
-
-const getIndentation = (lineText: string): string => {
-  const match = lineText.match(/^(\s*)/);
-  return match ? match[1] : "";
 };
 
 export const deactivate = () => {};
